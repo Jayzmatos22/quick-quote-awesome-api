c52d8b6057176e431956186b4000454b7483b051
Jailton Santos <jailtonmatos200@gmail.com>
2026-07-07 14:16:46 -0300

    feat: request state management

---
+++ app/hooks/currencies.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import { quoteService } from "../services/quoteService";
+
+
+// 5 minutos de cache para evitar chamadas desnecessárias à API
+export const useDailyCurrencies = (from: string, to: string, days: string) => {
+  return useQuery({
+    queryKey: ["dailyCurrencies", from, to, days],
+    queryFn: () => quoteService.getDailyQuotes(from, to, days),
+    enabled: !!from && !!to,
+    staleTime: 1000 * 60 * 5, // 5 minutos
+  });
+};
